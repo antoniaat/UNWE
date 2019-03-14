754adb35f3259db087761f923628079cdf155de7
antoniyaat <antoniyaata@gmail.com>
2019-03-14 12:02:40 +0200

    Add third homework (Application Systems Development)
    The exercise is not finished

---
+++ Fundamentals-for-Application-Systems-Development/01_ Types-Variables-and-Values-Arrays/Homework-03/Exercise-01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_01
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var egnStr = Console.ReadLine();
+            if (egnStr == null) return;
+
+            var birthdayDate = egnStr.Take(6).ToArray();
+            var year = int.Parse($"19{birthdayDate[0]}{birthdayDate[1]}");
+            var month = int.Parse($"{birthdayDate[2]}{birthdayDate[3]}");
+            var day = int.Parse($"{birthdayDate[4]}{birthdayDate[5]}");
+
+            var date = new DateTime(year, month, day);
+
+            Console.WriteLine(date);
+        }
+    }
+}
